392995fbfa59c698dbbf1961fb8eb1a5e9e37363
CatherineKeller <catherine.keller.oclock@gmail.com>
2023-02-27 15:51:05 +0100

    feat: add Joi schema for the route PUT

--- app/routers/adRouter.js
+++ app/routers/adRouter.js
@@ -6,13 +6,14 @@ const controllerHandler = require('../controllers/controllerHandler');
 
 const validate = require('../validations/validator');
 const { post: adPostSchema } = require('../validations/schemas/ad.schema');
+const { put: adPutSchema } = require('../validations/schemas/ad.schema');
 
 const authenticateToken = require('../middlewares/authenticateToken');
 
 adRouter.get('/user/:id([0-9]+)/ads', authenticateToken, controllerHandler(adController.getAdsByUserId));
 adRouter.post('/user/:id([0-9]+)/ads', authenticateToken, validate(adPostSchema, 'body'), controllerHandler(adController.createAdByUserId));
 adRouter.get('/ads', authenticateToken, controllerHandler(adController.getAllAds));
-adRouter.put('/ads/:id([0-9]+)', authenticateToken, controllerHandler(adController.updateAdById));
+adRouter.put('/ads/:id([0-9]+)', authenticateToken, validate(adPutSchema, 'body'), controllerHandler(adController.updateAdById));
 adRouter.delete('/ads/:id([0-9]+)', authenticateToken, controllerHandler(adController.deleteAdById));
 
 module.exports = adRouter;
