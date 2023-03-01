3069d1c337f2b6c54ff349881aae82c55f3693b5
CatherineKeller <catherine.keller.oclock@gmail.com>
2023-03-01 12:09:30 +0100

    doc: ad router

--- app/routers/adRouter.js
+++ app/routers/adRouter.js
@@ -10,7 +10,7 @@ const { put: adPutSchema } = require('../validations/schemas/ad.schema');
 
 const authenticateToken = require('../middlewares/authenticateToken');
 
-/** a Ad type
+/** a general Ad type
  * @typedef {object} Ad
  * @property {number} id - ad id
  * @property {string} title - ad title
@@ -22,13 +22,34 @@ const authenticateToken = require('../middlewares/authenticateToken');
  * @property {string} updated_at - ad update date
  */
 
-/** a create/modify Ad type
+/** a get Ad type
+ * @typedef {object} AdGet
+ * @property {number} id - ad id
+ * @property {string} title - ad title
+ * @property {string} content - ad content
+ * @property {string} city - ad city
+ * @property {string} postal_code - ad postal code
+ * @property {number} user_id - user id
+ * @property {array<User>} user - ad user with id first_name and last_name
+ * @property {string} created_at - ad creation date
+ * @property {string} updated_at - ad update date
+ */
+
+
+/** a create/modify Ad (for body) type
  * @typedef {object} AdCreateModify
  * @property {string} title - ad title
  * @property {string} content - ad content
  * @property {string} city - ad city
  * @property {string} postal_code - ad postal code
- */
+*/
+
+/** a get Ad type
+ * @typedef {object} User
+ * @property {number} id - user id
+ * @property {string} first_name - user first_name
+ * @property {string} last_name - user last_name
+ * /
 
 /**
  * GET /user/{id}/ads
@@ -53,9 +74,9 @@ adRouter.get('/user/:id([0-9]+)/ads', authenticateToken, controllerHandler(adCon
  * @tags Ads
  *
  * @param {number} id.path - user id
- * @param {Ad} request.body - ad
+ * @param {AdCreateModify} request.body - ad
  *
- * @return {AdCreateModify} 201 - success response
+ * @return {Ad} 201 - success response
  * @return {object} 500 - internal server error
  * @return {object} 401 - unauthorized
  * @return {object} 403 - forbidden
@@ -70,7 +91,7 @@ adRouter.post('/user/:id([0-9]+)/ads', authenticateToken, validate(adPostSchema,
  * @summary get all ads
  * @tags Ads
  *
- * @return {array<Ad>} 200 - success response
+ * @return {array<AdGet>} 200 - success response
  * @return {object} 500 - internal server error
  * @return {object} 401 - unauthorized
  *
@@ -85,9 +106,9 @@ adRouter.get('/ads', authenticateToken, controllerHandler(adController.getAllAds
  * @tags Ads
  *
  * @param {number} id.path - ad id
- * @param {Ad} request.body - post
+ * @param {AdCreateModify} request.body - post
  *
- * @return {AdCreateModify} 200 - success response
+ * @return {Ad} 200 - success response
  * @return {object} 500 - internal server error
  * @return {object} 401 - unauthorized
  * @return {object} 403 - forbidden
